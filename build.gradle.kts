bdab8649d5441b4bd6c19f0490d47e491b587a78
ark-65 <liuwufangzhou@vip.qq.com>
2026-07-25 10:24:48 +0800

    fix(ci): remove unavailable DataGrip verifier target

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,7 +112,6 @@ intellijPlatform {
     pluginVerification {
         ides {
             create(IntelliJPlatformType.CLion, "2023.1.7")
-            create(IntelliJPlatformType.DataGrip, "2023.1.7")
             create(IntelliJPlatformType.DataSpell, "2023.1.6")
             create(IntelliJPlatformType.GoLand, "2023.1.6")
             create(IntelliJPlatformType.IntellijIdeaCommunity, "2023.1.7")
